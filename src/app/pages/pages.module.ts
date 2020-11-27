a15401644396da3618457c3536e820dd85b00ea8
luvellana <luciananunezl99@gmail.com>
2020-11-26 23:25:44 -0400

    prueba

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,29 @@ import {CommonModule} from '@angular/common';
 
 import {PagesRoutingModule} from './pages-routing.module';
 import {PagesComponent} from './pages.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {path: '', redirectTo: 'nyc', pathMatch: 'full'},
+      {path: 'nyc', loadChildren: () => import('./nyc/nyc.module').then(m => m.NycModule)},
+      {path: 'rutas', loadChildren: () => import('./rutas/rutas.module').then(m => m.RutasModule)},
+      {path: 'sobre-lpb', loadChildren: () => import('./sobre-lpb/sobre-lpb.module').then(m => m.SobreLPBModule)}
+    ]
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule, PagesRoutingModule
+    CommonModule, 
+    PagesRoutingModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [ PagesComponent
+  declarations: [ 
+    PagesComponent
     ]
 })
 export class PagesModule { }
