9046fb04be11e35151f88353572555dd8ddc8df7
luvellana <luciananunezl99@gmail.com>
2020-11-27 19:19:41 -0400

    intento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { PagesComponent } from './pages/pages.component';
-import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from "./app.component";
+import { PagesComponent } from "./pages/pages.component";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
- {path: '', redirectTo: 'pages', pathMatch: 'full'},
- {path: 'pages', component: PagesComponent /*loadChildren: () => import("./pages/pages.module").then(m => m.PagesModule)*/}
+  { path: "", redirectTo: "pages", pathMatch: "full" },
+  {
+    path: "pages",
+    loadChildren: () =>
+      import("./pages/pages.module").then(
+        m => m.PagesModule
+      ) /*component: PagesComponent */
+  }
 ];
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
-    RouterModule.forRoot(routes)],
-  declarations: [ AppComponent],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
